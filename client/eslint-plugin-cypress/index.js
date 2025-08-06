f6588178ac025f37851ca8aa6a237190b8261435
sufi-hassan-tools <122306352+sufi-hassan-tools@users.noreply.github.com>
2025-08-06 15:59:18 +0000

    chore: enable Cypress ESLint plugin

---
+++ client/eslint-plugin-cypress/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  configs: {},
+  rules: {},
+  environments: {
+    globals: {
+      globals: {
+        cy: false,
+        Cypress: false,
+        assert: false,
+        expect: false,
+        before: false,
+        beforeEach: false,
+        after: false,
+        afterEach: false,
+        describe: false,
+        it: false,
+        context: false,
+      },
+    },
+  },
+};
